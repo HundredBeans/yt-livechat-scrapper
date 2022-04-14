22d60698cbdc100072176c57582574e5c75b8e4a
Mohammad Daffa <m.daffa100@yahoo.com>
2022-04-14 22:27:38 +0700

    feat: add telegram notification

--- liveChat.js
+++ liveChat.js
@@ -1,25 +1,31 @@
 const { LiveChat } = require("youtube-chat");
-const { getChannelCollectionById } = require("./utils");
+const { getChannelCollectionById, sendNotification } = require("./utils");
 
 /**
  * Function to initiate liveChat instance via channelId
  * @param {string} channelId 
  */
-const initiateLiveChatInstance = async (channelId) => {
+const initiateLiveChatInstance = async (channelId, channelName, channelLink) => {
   const liveChat = new LiveChat({ channelId });
 
   const channelCollection = getChannelCollectionById(channelId);
 
   liveChat.on("start", liveId => {
     console.log("live started with liveId:", liveId)
+    const message = `**Livestream Started** in Channel: [${channelName}](${channelLink}). Livestream [here](https://www.youtube.com/watch?v=${liveId})`
+    sendNotification(message)
   });
 
   liveChat.on("error", err => {
     console.log("live error", err)
+    const message = `**Livestream error** in Channel: [${channelName}](${channelLink}). Cause: *${err.message}*`
+    sendNotification(message)
   });
 
   liveChat.on("end", reason => {
     console.log("live ended", reason)
+    const message = `**Livestream just ended** in Channel: [${channelName}](${channelLink}) with liveId: ${liveChat.liveId}`
+    sendNotification(message)
   });
 
   liveChat.on("chat", chatItem => {
--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const { db } = require("./db");
+const https = require("https");
 
 const getAllChannels = async () => {
   const channels = []
@@ -16,7 +17,17 @@ const getAllChannels = async () => {
 
 const getChannelCollectionById = (id) => db.collection('channels').doc(id)
 
+const sendNotification = (message) => {
+  const token = process.env.TELEGRAM_BOT_TOKEN;
+  const chatId = process.env.TELEGRAM_CHAT_ID;
+  const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${message}&parse_mode=markdown`;
+  https.get(url).on("error", (e) => {
+    console.log("Send Telegram Message error", e)
+  });
+}
+
 module.exports = {
   getAllChannels,
-  getChannelCollectionById
+  getChannelCollectionById,
+  sendNotification
 }
